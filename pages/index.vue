228319b3f87d1193903965f7fc8e1d2fbd3a9f82
Dr hfx <balloigor66@gmail.com>
2022-09-07 10:50:10 +0000

    add login page

--- pages/index.vue
+++ pages/index.vue
@@ -17,13 +17,37 @@
           </div>
           <div class="lg:w-2/9 flex items-center gap-4 border-l border-gray-300 px-6 py-6">
             <NuxtLink to="/contact" class="text-lg font-semibold whitespace-nowrap text-eng-500">Contactez-nous</NuxtLink>
-            <NuxtLink to="/login" class="bg-eng-700 shadow-md text-white text-lg font-medium rounded-lg px-6 py-2 whitespace-nowrap">S'inscrire</NuxtLink>
-            <NuxtLink to="/register" class="bg-eng-300 shadow-md text-white text-lg font-medium rounded-lg px-6 py-2 whitespace-nowrap">Se connecter</NuxtLink>
+            <NuxtLink to="/login" class="bg-eng-700 shadow-md text-white text-lg font-medium rounded px-6 py-2 whitespace-nowrap">S'inscrire</NuxtLink>
+            <NuxtLink to="/register" class="bg-eng-300 shadow-md text-white text-lg font-medium rounded px-6 py-2 whitespace-nowrap">Se connecter</NuxtLink>
           </div>
         </navbar>
+
       </header>
 
-      <div class="h-[100vh] bg-yellow-600 mt-[500px]"></div>
+      <div class="bg-white px-6 pt-8 lg:px-16 mt-[400px] pb-64 font-eng">
+        <div class="bg-eng-300 px-6 py-2 rounded flex justify-between items-center">
+          <h1 class="text-xl text-white">Ce que nous faisons - Énergie industrie et ressources - <span class="font-medium">Énergie</span></h1>
+          <button class="p-4 font-semibold text-lg bg-gradient-to-r from-yellow-500 to-yellow-400 rounded">Demandez un devis</button>
+        </div>
+
+        <div class="mt-10 mb-6">
+          <h1 class="text-red-500 text-5xl font-extrabold camelcase">Ce que nous faisons</h1>
+          <h2 class="text-xl mt-6">Nous développons des solutions d’ingénierie créatives, complètes et durables pour un avenir où les collectivités peuvent s’épanouir. Munis d’une profonde compréhension des complexités locales,
+            de talents de renommée mondiale et d’un leadership proactif, nous planifions, concevons et gérons des projets qui auront un impact positif à long terme.</h2>
+        </div>
+
+        <div class="mt-10 mb-6">
+          <h1 class="text-red-500 text-5xl font-extrabold camelcase">Energie Photovoltaîque</h1>
+          <h2 class="text-xl mt-6"><strong>Energie4All</strong> aide les clients dans tous les secteurs d’activité à répondre à la demande croissante en énergie, à réduire leur consommation d’énergie et
+            leurs émissions de gaz à effet de serre. Nous fournissons également des solutions énergétiques qui contribueront à la création d’un monde durable.</h2>
+          <h2 class="text-xl mt-6">La conversion de lumière solaire en électricité suscite un intérêt grandissant, car elle pourrait représenter une solution à notre dépendance aux combustibles fossiles et donc favoriser la sécurité énergétique,
+            réduire les émissions de carbone et autres émissions nocives contribuant aux changements climatiques, ainsi que stabiliser les coûts énergétiques en vue d’atteindre la durabilité sur les plans financier et environnemental.
+            De plus, les projets d’énergie solaire s’exécutent plus facilement que les grandes initiatives hydroélectriques, nucléaires ou la plupart des initiatives thermiques. À l’échelle commerciale,
+            ils facilitent l’approvisionnement en énergie pour répondre aux besoins des endroits sous-développés ou éloignés, où l’électricité est une denrée rare.</h2>
+        </div>
+      </div>
+
+
     </div>
   </div>
 </template>
---
+++ pages/login.vue
@@ -0,0 +1,96 @@
+<template>
+  <div>
+    <div class="background-header bg-no-repeat bg-cover bg-center relative">
+      <div class="absolute bg-gradient-to-l from-gray-900 to-purple-800 opacity-75 inset-0 z-0"></div>
+      <div>
+        <div class="min-h-screen flex justify-center">
+
+          <div class="grid grid-cols-2 gap-4 items-center z-10">
+            <div class="max-w-lg text-center sm:text-left">
+              <h2 class="text-4xl font-bold text-gray-100 tracking-tight">
+                Lorem ipsum,<br class="hidden sm:block lg:hidden"> dolor sit amet consectetur adipisicing elit
+
+
+              </h2>
+
+              <p class="mt-4 text-gray-300">
+                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Voluptas
+                veritatis illo placeat harum porro optio fugit a culpa sunt id!
+              </p>
+            </div>
+
+            <div class="mx-0 max-w-xl  flex bg-white">
+              <div class="swiper-container flex-col flex  self-center">
+                <div class="swiper-wrapper">
+                  <div class="swiper-slide">
+                    <blockquote class="p-8  text-center">
+                      <div class="">
+                        <img
+                          src="/storage/avatars/njkIbPhyZCftc4g9XbMWwVsa7aGVPajYLRXhEeoo.jpg"
+                          alt="aji"
+                          class="object-cover w-16 h-16 rounded-full mx-auto"
+                        />
+
+                        <div class="text-sm my-3">
+                          <p class="font-medium text-gray-900">Ajimon</p>
+                          <p class="mt-1 text-gray-500">CEO Of Marketing </p>
+                        </div>
+                        <div class="flex space-x-0.5 justify-center text-yellow-400">
+                          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                          <svg xmlns="http://www.w3.org/2000/svg" class="w-4 h-4" viewBox="0 0 20 20" fill="currentColor">
+                            <path d="M9.049 2.927c.3-.921 1.603-.921 1.902 0l1.07 3.292a1 1 0 00.95.69h3.462c.969 0 1.371 1.24.588 1.81l-2.8 2.034a1 1 0 00-.364 1.118l1.07 3.292c.3.921-.755 1.688-1.54 1.118l-2.8-2.034a1 1 0 00-1.175 0l-2.8 2.034c-.784.57-1.838-.197-1.539-1.118l1.07-3.292a1 1 0 00-.364-1.118L2.98 8.72c-.783-.57-.38-1.81.588-1.81h3.461a1 1 0 00.951-.69l1.07-3.292z"></path>
+                          </svg>
+                        </div>
+                      </div>
+
+                      <p class="relative mt-4 text-gray-400">
+                        <span class="text-xl text-gray-300">&ldquo;</span>
+
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit. Magni
+                        assumenda officiis sit amet itaque eveniet accusantium corporis
+                        tempora, soluta perspiciatis rerum, ratione animi nemo inventore
+                        repellat, commodi in esse quisquam.
+
+                        <span class="text-xl text-gray-300">&rdquo;</span>
+                      </p>
+                    </blockquote>
+                  </div>
+
+                </div>
+
+                <div class="mt-12 swiper-pagination"></div>
+              </div>
+            </div>
+          </div>
+
+
+
+        </div>
+      </div>
+    </div>
+
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style scoped>
+.background-header {
+  background-image: url(https://images.unsplash.com/photo-1613665813446-82a78c468a1d?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=858&q=80);
+}
+</style>
